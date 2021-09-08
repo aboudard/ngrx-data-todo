6bff09727017d85e51339ac1188e1ff97ed02bc5
Alain Boudard <coco.boudard@gmail.com>
2021-09-09 00:13:40 +0200

    feat: update vers ng 12
    BREAK: mise à jour ng 12

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AppStoreModule } from './store/app-store.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { AppStoreModule } from './store/app-store.module';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    AppStoreModule
+    AppStoreModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
