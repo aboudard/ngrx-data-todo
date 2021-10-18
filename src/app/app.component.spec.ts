5695c208e70c0fde3e5db0cf08525f364b55b094
Alain Boudard <alain_boudard@groupe-sma.fr>
2021-10-18 13:01:57 +0200

    tests: coverage improve

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,14 @@ import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { AppState } from './reducers';
 import { TodosService } from './services/todos.service';
 import { createSpyFromClass } from 'jasmine-auto-spies'
+import { SubscriberSpy, subscribeSpyTo } from '@hirez_io/observer-spy';
+import { increment } from './actions/counter.actions';
 
 describe('AppComponent', () => {
 
   let componentUnderTest: AppComponent;
   let storeSpy: MockStore<Partial<AppState>>;
-
+  let observerSpy: SubscriberSpy<any>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
@@ -25,10 +27,18 @@ describe('AppComponent', () => {
     }).compileComponents();
     componentUnderTest = TestBed.inject(AppComponent);
     storeSpy = TestBed.inject(MockStore);
+    observerSpy = undefined;
   }));
 
   it('should create the app', () => {
     expect(componentUnderTest).toBeTruthy();
+  });
+
+  it('should dispatch the counter method', () => {
+    componentUnderTest.ngOnInit();
+    observerSpy = subscribeSpyTo(storeSpy.scannedActions$);
+    const expectedAction = increment();
+    expect(observerSpy.getValues()).toEqual([expectedAction]);
   });
 
 
